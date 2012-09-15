da8322a5cdcbb5a6a2022b7d8a2ad5040f50a3bc
Varuni Gang <varuni.gang@gmail.com>
2012-09-15 15:20:20 -0400

    initial

---
+++ src/main/java/edu/cmu/lti/f12/hw0/hw0_vgang/App.java
@@ -0,0 +1,13 @@
+package edu.cmu.lti.f12.hw0.hw0_vgang;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
